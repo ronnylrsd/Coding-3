779feaa3a4fbb062b29fe235b281f1c307be89f8
matheus albuquerque <matheus.albuquerque@hotmail.com>
2021-03-20 21:07:43 -0300

    segundo teste

--- Documents/NetBeansProjects/projetotestegit2/src/projetotestegit2/Projetotestegit2.java
+++ Documents/NetBeansProjects/projetotestegit2/src/projetotestegit2/Projetotestegit2.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package projetotestegit2;
 
-/**
- *
- * @author Pichau
- */
+package projetotestegit2;
+import java.util.Scanner;
+
 public class Projetotestegit2 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+    
+    Scanner input = new Scanner(System.in);
+    
+   System.out.println("escreva abaixo");
+   String nome = input.nextLine();
+    
     }
     
 }
